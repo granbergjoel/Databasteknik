22f4d2fc3ed1ac5af774950795505c89c4e1883e
Joel Granberg <granberg.joel@gmail.com>
2021-02-04 15:32:27 +0100

    lektion 02

---
+++ lektion-02/create.php
@@ -0,0 +1,59 @@
+<?php
+
+/*****************************
+* 
+*        Create
+*     Skapa en ny kontakt
+* 
+*****************************/
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    require_once ("database.php");
+    /*
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+    */
+$name = $_POST['name'];
+$tel = $_POST['tel'];
+
+// Skapa en SQL-sats (förbereda ett statement)
+$stmt = $conn->prepare("INSERT INTO contacts (name, tel)
+                    VALUES (:name , :tel )");
+
+//Binda parametrar (binda variabler med platshållare)
+$stmt->bindParam(':name' , $name);
+$stmt->bindParam(':tel' , $tel);
+
+//Kör SQL-satsen
+$stmt->execute();
+
+//Hämta senaste ID som infogats med A_I
+$last_id=$conn->lastInsertId();
+
+
+$message= "<div class='alert alert-success' role='alert'>
+<p>$name har sparats i databasen med id=$last_id</p>
+</div>";
+
+
+}
+    ?>
+
+
+<form action="#" class="row" method="post">
+
+<div class="col-md-4 my-2">
+    <input type="text" class="form-control" name="name">
+</div>
+
+<div class="col-md-4 my-2">
+    <input type="text" class="form-control" name="tel">
+</div>
+
+<div class="col-md-4 my-2">
+    <input type="submit" value="Lägg till" class="form-control btn btn-outline-info">
+</div>
+
+</form>
+
+<?php  if (isset($message)) echo $message; ?>
---
+++ lektion-02/header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="sv">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap.css">
+    <title>Joels Telefonlista</title>
+</head>
+
+<body class="container">
+    <h1 class="text-center">
+        <a href="index.php">
+            Joels Telefonlista
+        </a>
+    </h1>
